3eab91687e8fd5b76e9d879018ad1776dcb75208
abhishtagatya <abhishtagatya@yahoo.com>
2024-01-11 19:04:08 +0100

    Added Command Arguments for M5 Model CLF Dense

--- m5_clf_dense.py
+++ m5_clf_dense.py
@@ -1,4 +1,5 @@
-import glob
+import argparse
+import os
 
 from lib.dataset.audio_mnist import (
     AudioMNIST, RESAMPLE_16K_TO_8K, collate_fn
@@ -63,7 +64,18 @@ def test(model, loader, epoch):
 
 
 if __name__ == '__main__':
-    DIR = "./module/AudioMNIST/data/*/*.wav"
+
+    # Get Script Argument
+    parser = argparse.ArgumentParser(description="Wav2Vec2.0 ASR Inference")
+    parser.add_argument("dirpath", help="Audio Dataset Folder", type=str)
+    parser.add_argument("lr", help="Optimizer Learning Rate", type=str)
+    parser.add_argument("weight_decay", help="Optimizer Weight Decay", type=str)
+    parser.add_argument("scheduler_step", help="Scheduler Step Size", type=str)
+    parser.add_argument("scheduler_gamma", help="Scheduler Gamma", type=str)
+    parser.add_argument("epoch", help="Epoch", type=str)
+    args = parser.parse_args()
+
+    DIR = args.dirpath
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     train_ds, test_ds = AudioMNIST.load_from_dir(DIR, transform=RESAMPLE_16K_TO_8K, split=0.8)
@@ -74,14 +86,18 @@ if __name__ == '__main__':
     model.to(device)
 
     # Fine Tune the Parameters to get better results
-    optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0005)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    optimizer = optim.Adam(model.parameters(), lr=float(args.lr), weight_decay=float(args.weight_decay))
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=int(args.scheduler_step), gamma=float(args.scheduler_gamma))
 
     log_interval = 20
-    n_epoch = 10
+    n_epoch = int(args.epoch)
 
     for epoch in range(n_epoch):
         train(model, train_loader, epoch, log_interval)
         test(model, train_loader, epoch)
         test(model, test_loader, epoch)
         scheduler.step()
+
+    save_as = f"m5_10+{args.learning_rate}-{args.weight_decay}-sch{args.scheduler_gamma}.pth.tar"
+    checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+    torch.save(checkpoint, save_as)
